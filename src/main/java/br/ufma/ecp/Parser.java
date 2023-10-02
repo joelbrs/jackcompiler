9e4efaacb24aab4819780a68d5ff52cd6d1513f3
Joel Filho <joelfilho02812@gmail.com>
2023-10-02 14:57:32 -0300

    refactor: nova estrutura do parser p/ analisador sintático

--- src/main/java/br/ufma/ecp/Parser.java
+++ src/main/java/br/ufma/ecp/Parser.java
@@ -4,39 +4,16 @@ import br.ufma.ecp.token.Token;
 import br.ufma.ecp.token.TokenType;
 
 public class Parser {
+    private static class ParseError extends RuntimeException {};
     private final Scanner scan;
     private Token currentToken;
-    
+    private Token peekToken;
+    private StringBuilder xmlOutput = new StringBuilder();
+
     public Parser (byte[] input) {
         scan = new Scanner(input);
-        currentToken = scan.nextToken();
+        nextToken();
     }
-
-    public void parse () {
-        expr();
-    }
-
-    void expr() {
-        number();
-        oper();
-    }
-
-    void number () {
-        System.out.println(currentToken.getLexeme());
-        match(TokenType.NUMBER);
-    }
-
-    private void nextToken () {
-        currentToken = scan.nextToken();
-    }
-
-   private void match(TokenType t) {
-        if (currentToken.getType() == t) {
-            nextToken();
-        }else {
-            throw new Error("syntax error");
-        }
-   }
 
     void oper () {
         if (currentToken.getType() == TokenType.PLUS) {
@@ -54,8 +31,68 @@ public class Parser {
         }
     }
 
-    public String VMOutput() {
-        return "";
+    public String XMLOutput() {
+        return xmlOutput.toString();
     }
 
+    private void printNonTerminal(String nterminal) {
+        xmlOutput.append(String.format("<%s>\r\n", nterminal));
+    }
+
+    boolean peekTokenIs(TokenType type) {
+        return peekToken.getType() == type;
+    }
+
+    boolean currentTokenIs(TokenType type) {
+        return currentToken.getType() == type;
+    }
+
+    private void expectPeek(TokenType... types) {
+        for (TokenType type: types) {
+            if (peekToken.getType() == type) {
+                expectPeek(type);
+                return;
+            }
+        }
+        throw error(peekToken, "Expected a statement");
+    }
+
+    private void expectPeek(TokenType type) {
+        if (peekToken.getType() == type) {
+            nextToken();
+            xmlOutput.append(String.format("%s\r\n", currentToken.toString()));
+            return;
+        }
+        throw error(peekToken, "Expected " + type.name());
+    }
+
+    private static void report(int line, String where, String message) {
+        System.err.println("[line " + line + " ] Error" + where + ": " + message);
+    }
+
+    private ParseError error(Token token, String message) {
+        if (token.getType() == TokenType.EOF) {
+            report(token.getLine(), "at end", message);
+        } else {
+            report(token.getLine(), "at '" + token.getLexeme() + "'", message);
+        }
+        return new ParseError();
+    }
+    void number () {
+        System.out.println(currentToken.getLexeme());
+        match(TokenType.NUMBER);
+    }
+
+    private void nextToken () {
+        currentToken = peekToken;
+        peekToken = scan.nextToken();
+    }
+
+    private void match(TokenType t) {
+        if (currentToken.getType() == t) {
+            nextToken();
+        }else {
+            throw new Error("syntax error");
+        }
+    }
 }
