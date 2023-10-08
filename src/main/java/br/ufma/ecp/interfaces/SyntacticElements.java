47e34e4db6e83c1bc206d9be16ea4b2d6bda7ff8
Joel Filho <joelfilho02812@gmail.com>
2023-10-08 13:07:09 -0300

    refactor: interface elementos sintaticos

--- src/main/java/br/ufma/ecp/interfaces/SyntacticElements.java
+++ src/main/java/br/ufma/ecp/interfaces/SyntacticElements.java
@@ -1,9 +1,19 @@
 package br.ufma.ecp.interfaces;
 
 public interface SyntacticElements {
+    void parseExpression();
+    void parseTerm();
+    void parseSubRoutineCall();
+    void parseExpressionList();
+    void parseStatements();
+    void parseLet();
+    void parseIf();
+    void parseWhile();
+    void parseDo();
+    void parseReturn();
     void parseClassVarDec();
     void parseVarDec();
     void parseSubRoutineBody();
-    void subRoutineDec();
-
+    void parseSubRoutineDec();
+    void parseParameterList();
 }
